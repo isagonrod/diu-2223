5f674854bcb18666b447f744fbbe753464337d2f
Miguel Ángel Ruiz <miguelangel.ruiz@m-bet.com>
2022-10-23 20:39:17 +0200

    Agenda: Añadido script SQL.

---
+++ agenda.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE agenda;
+
+USE agenda;
+
+CREATE TABLE IF NOT EXISTS person (
+	id INT(2) NOT NULL AUTO-INCREMENTED, 
+	firstName VARCHAR(50), 
+	lastName VARCHAR(50), 
+	street VARCHAR(50), 
+	city VARCHAR(50),
+	postalCode INT(5), 
+	birthday DATE, 
+	PRIMARY KEY(id)
+) ENGINE=InnoDB;
