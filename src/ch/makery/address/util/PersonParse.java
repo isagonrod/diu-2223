0802cbf9d11616068cc825d948f89a8322daa48f
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-10-25 19:07:18 +0200

    AGENDA: simplificado el PersonParse.

--- src/ch/makery/address/util/PersonParse.java
+++ src/ch/makery/address/util/PersonParse.java
@@ -4,45 +4,27 @@ import ch.makery.address.model.Person;
 import ch.makery.address.model.PersonVO;
 
 public class PersonParse {
-	// TODO: Clase para pasar de PersonVO a Person
-	private Person person;
-	private PersonVO personVO;
-
-	public Person getPerson() {
-		return this.person;
+	public static PersonVO parseToPersonVO(Person person) {
+		PersonVO personVO = new PersonVO();
+		personVO.setId(person.getId());
+		personVO.setFirstName(person.getFirstName());
+		personVO.setLastName(person.getLastName());
+		personVO.setStreet(person.getStreet());
+		personVO.setCity(person.getCity());
+		personVO.setPostalCode(person.getPostalCode());
+		personVO.setBirthday(person.getBirthday());
+		return personVO;
 	}
 
-	public void setPerson(Person person) {
-		this.person = person;
-	}
-
-	public PersonVO getPersonVO() {
-		return this.personVO;
-	}
-
-	public void setPersonVO(PersonVO personVO) {
-		this.personVO = personVO;
-	}
-
-	public PersonParse() {}
-
-	public void parseToPersonVO() {
-		this.personVO.setId(this.person.getId());
-		this.personVO.setFirstName(this.person.getFirstName());
-		this.personVO.setLastName(this.person.getLastName());
-		this.personVO.setStreet(this.person.getStreet());
-		this.personVO.setCity(this.person.getCity());
-		this.personVO.setPostalCode(this.person.getPostalCode());
-		this.personVO.setBirthday(this.person.getBirthday());
-	}
-
-	public void parseToPerson() {
-		this.person.setId(this.personVO.getId());
-		this.person.setFirstName(this.personVO.getFirstName());
-		this.person.setLastName(this.personVO.getLastName());
-		this.person.setStreet(this.personVO.getStreet());
-		this.person.setCity(this.personVO.getCity());
-		this.person.setPostalCode(this.personVO.getPostalCode());
-		this.person.setBirthday(this.personVO.getBirthday());
+	public static Person parseToPerson(PersonVO personVO) {
+		Person person = new Person();
+		person.setId(personVO.getId());
+		person.setFirstName(personVO.getFirstName());
+		person.setLastName(personVO.getLastName());
+		person.setStreet(personVO.getStreet());
+		person.setCity(personVO.getCity());
+		person.setPostalCode(personVO.getPostalCode());
+		person.setBirthday(personVO.getBirthday());
+		return person;
 	}
 }
