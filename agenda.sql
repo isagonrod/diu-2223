15f329dce233a30e1af6e36806c0c9b16114f942
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-02 16:31:40 +0100

    AGENDA: Retocado script

--- agenda.sql
+++ agenda.sql
@@ -3,12 +3,12 @@ CREATE DATABASE agenda;
 USE agenda;
 
 CREATE TABLE IF NOT EXISTS person (
-	id INT(50) NOT NULL AUTO_INCREMENT,
+	idPerson INT NOT NULL AUTO_INCREMENT,
 	firstName VARCHAR(50), 
 	lastName VARCHAR(50), 
 	street VARCHAR(50), 
 	city VARCHAR(20),
 	postalCode INT(5), 
 	birthday DATE, 
-	PRIMARY KEY(id)
+	PRIMARY KEY(idPerson)
 ) ENGINE=InnoDB;
