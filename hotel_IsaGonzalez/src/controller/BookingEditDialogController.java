70bf599a8fbc28abc9a02a5b145eb49374d1be5c
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-06 18:14:36 +0100

    HOTEL: Creado controladores de las ventanas de edición

--- hotel_IsaGonzalez/src/controller/BookingEditDialogController.java
+++ hotel_IsaGonzalez/src/controller/BookingEditDialogController.java
@@ -1,4 +1,119 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import model.Booking;
+import model.HotelModel;
+import org.controlsfx.dialog.Dialogs;
+import util.DateUtil;
+
+import java.awt.*;
+
 public class BookingEditDialogController {
+    @FXML
+    private TextField codReservaField;
+    @FXML
+    private TextField fechaLlegadaField;
+    @FXML
+    private TextField fechaSalidaField;
+    @FXML
+    private TextField numHabitacionesField;
+    @FXML
+    private TextField tipoHabitacionField;
+    @FXML
+    private TextField fumadorField;
+    @FXML
+    private TextField regimenAlojamientoField;
+
+    private Stage dialogStage;
+    private Booking booking;
+    private boolean okClicked = false;
+    private boolean isNew;
+
+    @FXML
+    private void initialize() {}
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setBooking(Booking booking, boolean isNew) {
+        this.booking = booking;
+        this.isNew = isNew;
+
+        codReservaField.setText(String.valueOf(booking.getCodReserva()));
+        fechaLlegadaField.setText(DateUtil.format(booking.getFechaLlegada()));
+        fechaSalidaField.setText(DateUtil.format(booking.getFechaSalida()));
+        numHabitacionesField.setText(String.valueOf(booking.getNumHabitaciones()));
+        tipoHabitacionField.setText(booking.getTipoHabitacion());
+        fumadorField.setText(String.valueOf(booking.isFumador()));
+        regimenAlojamientoField.setText(booking.getRegimenAlojamiento());
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    @FXML
+    private void pushOk() {
+        if (isInputValid()) {
+            try {
+                booking.setCodReserva(Integer.parseInt(codReservaField.getText()));
+                booking.setFechaLlegada(DateUtil.parse(fechaLlegadaField.getText()));
+                booking.setFechaSalida(DateUtil.parse(fechaSalidaField.getText()));
+                booking.setNumHabitaciones(Integer.parseInt(numHabitacionesField.getText()));
+                booking.setTipoHabitacion(tipoHabitacionField.getText());
+                booking.setFumador(Boolean.parseBoolean(fumadorField.getText()));
+                booking.setRegimenAlojamiento(regimenAlojamientoField.getText());
+                if (isNew) {
+                    new HotelModel().saveBooking(booking);
+                } else {
+                    new HotelModel().editBooking(booking);
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    private void pushCancel() {
+        dialogStage.close();
+    }
+
+    private boolean isInputValid() {
+        String errorMessage = "";
+        if (codReservaField.getText() == null || codReservaField.getText().length() == 0) {
+            errorMessage += "Código de reserva no válido";
+        }
+        if (fechaLlegadaField.getText() == null || fechaLlegadaField.getText().length() == 0) {
+            errorMessage += "Fecha de llegada no válida";
+        }
+        if (fechaLlegadaField.getText() == null || fechaSalidaField.getText().length() == 0) {
+            errorMessage += "Fecha de salida no válida";
+        }
+        if (numHabitacionesField.getText() == null || numHabitacionesField.getText().length() == 0) {
+            errorMessage += "Número de habitaciones no válido";
+        }
+        if (tipoHabitacionField.getText() == null || tipoHabitacionField.getText().length() == 0) {
+            errorMessage += "Tipo de habitación no válido";
+        }
+        if (fumadorField.getText() == null || fumadorField.getText().length() == 0) {
+            errorMessage += "Elección de fumador no válida";
+        }
+        if (regimenAlojamientoField.getText() == null || regimenAlojamientoField.getText().length() == 0) {
+            errorMessage += "Régimen de alojamiento no válido";
+        }
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Dialogs.create()
+                    .title("Campos inválidos")
+                    .masthead("Por favor, corrija los campos inválidos")
+                    .message(errorMessage)
+                    .showError();
+            return false;
+        }
+    }
 }
--- hotel_IsaGonzalez/src/controller/CustomerEditDialogController.java
+++ hotel_IsaGonzalez/src/controller/CustomerEditDialogController.java
@@ -1,4 +1,109 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import model.Customer;
+import model.HotelModel;
+import java.awt.*;
+import org.controlsfx.dialog.Dialogs;
+
 public class CustomerEditDialogController {
+    @FXML
+    private TextField dniField;
+    @FXML
+    private TextField nombreField;
+    @FXML
+    private TextField apellidosField;
+    @FXML
+    private TextField direccionField;
+    @FXML
+    private TextField localidadField;
+    @FXML
+    private TextField provinciaField;
+
+    private Stage dialogStage;
+    private Customer customer;
+    private boolean okClicked = false;
+    private boolean isNew;
+
+    @FXML
+    private void initialize() {}
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    public void setCustomer(Customer customer, boolean isNew) {
+        this.customer = customer;
+        this.isNew = isNew;
+
+        dniField.setText(customer.getDni());
+        nombreField.setText(customer.getNombre());
+        apellidosField.setText(customer.getApellidos());
+        direccionField.setText(customer.getDireccion());
+        localidadField.setText(customer.getLocalidad());
+        provinciaField.setText(customer.getProvincia());
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    @FXML
+    public void pushOk() {
+        if (isInputValid()) {
+            try {
+                customer.setDni(dniField.getText());
+                customer.setNombre(nombreField.getText());
+                customer.setApellidos(apellidosField.getText());
+                customer.setDireccion(direccionField.getText());
+                customer.setLocalidad(localidadField.getText());
+                customer.setProvincia(provinciaField.getText());
+                if (isNew) {
+                    new HotelModel().saveCustomer(customer);
+                } else {
+                    new HotelModel().editCustomer(customer);
+                }
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+
+    @FXML
+    public void pushCancel() {
+        dialogStage.close();
+    }
+
+    public boolean isInputValid() {
+        String errorMessage = "";
+        if (dniField.getText() == null || dniField.getText().length() == 0) {
+            errorMessage += "DNI no válido\n";
+        }
+        if (nombreField.getText() == null || nombreField.getText().length() == 0) {
+            errorMessage += "Nombre no válido\n";
+        }
+        if (apellidosField.getText() == null || apellidosField.getText().length() == 0) {
+            errorMessage += "Apellido/s no válido/s\n";
+        }
+        if (direccionField.getText() == null || direccionField.getText().length() == 0) {
+            errorMessage += "Dirección no válida\n";
+        }
+        if (localidadField.getText() == null || localidadField.getText().length() == 0) {
+            errorMessage += "Localidad no válida\n";
+        }
+        if (provinciaField.getText() == null || provinciaField.getText().length() == 0) {
+            errorMessage += "Provincia no válida\n";
+        }
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Dialogs.create()
+                    .title("Campos inválidos")
+                    .masthead("Por favor, corrige los campos inválidos")
+                    .message(errorMessage)
+                    .showError();
+            return false;
+        }
+    }
 }
