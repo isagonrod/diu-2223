17cf4bdf1dc2f59d75f1bce1f7d156ee41647f91
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-11-06 17:19:47 +0100

    HOTEL: Creado el controlador de estadísticas de reservas por meses

--- hotel_IsaGonzalez/src/controller/StadisticsController.java
+++ hotel_IsaGonzalez/src/controller/StadisticsController.java
@@ -1,4 +1,43 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
+import model.Booking;
+
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class StadisticsController {
+    @FXML
+    private BarChart<String, Integer> barChart;
+    @FXML
+    private CategoryAxis xAxis;
+    private ObservableList<String> monthNames = FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() {
+        String[] months = DateFormatSymbols.getInstance(Locale.getDefault()).getMonths();
+        monthNames.addAll(Arrays.asList(months));
+        xAxis.setCategories(monthNames);
+        xAxis.setTickLabelRotation(75.0);
+    }
+
+    public void setBookingData(List<Booking> bookings) {
+        int[] monthCounter = new int[12];
+        for (Booking booking : bookings) {
+            int monthBooking = booking.getNumHabitaciones();
+            monthCounter[monthBooking]++;
+        }
+        XYChart.Series<String, Integer> series = new XYChart.Series<>();
+        for (int i = 0; i < monthCounter.length; i++) {
+            series.getData().add(new XYChart.Data<>(monthNames.get(i), monthCounter[i]));
+        }
+        barChart.getData().add(series);
+    }
 }
