af546b03030de07c7cbfc829bb8d4860d36c053f
Isa Gonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-12-14 12:32:35 +0100

    REACT: Pasos 4, 5, 6 y 7

--- miprimerappreact/src/App.js
+++ miprimerappreact/src/App.js
@@ -1,15 +1,44 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import HelloComponent from "./components/HelloComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hola</h1>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <HelloComponent></HelloComponent>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+	constructor() {
+		super(0);
+		this.state = {
+			name: 'Isa'
+		}
+	}
+
+	changeName = (event) => {
+		this.setState({
+			name: event.target.value
+		})
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo"/>
+					<HelloComponent nombre = {this.state.name}></HelloComponent>
+					<input value={this.state.name} onChange={this.changeName}/>
+				</header>
+			</div>
+		)
+	}
 }
 
 export default App;
