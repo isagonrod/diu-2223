cc73223640057c2b4783860c679a99e94cf09e60
Isa Gonzalez <isabelmariagonzalezrodriguez@gmail.com>
2022-12-20 12:44:49 +0100

    CALCULADORA REACT: Avance

--- calculadora_react/src/components/CalculatorButtons.js
+++ calculadora_react/src/components/CalculatorButtons.js
@@ -6,33 +6,33 @@ export default function CalculatorButtons(props) {
 	return (
 		<div className="container">
 			<div>
-				<button onClick={calculate(props, "ac")}>AC</button>
-				<button onClick={calculate(props, "negativeNumber")}>+/-</button>
-				<button onClick={calculate(props, "perCent")}>%</button>
-				<button className="operacion" onClick={calculate(props, "divide")}>÷</button>
+				<button onClick={calculate(props, this)} name="AC">AC</button>
+				<button onClick={calculate(props, this)} name="+/-">+/-</button>
+				<button onClick={calculate(props, this)} name="%">%</button>
+				<button className="operation" onClick={calculate(props, this)} name="÷">÷</button>
 			</div>
 			<div>
 				<button onClick={isNumber()} name="7">7</button>
 				<button onClick={isNumber()} name="8">8</button>
 				<button onClick={isNumber()} name="9">9</button>
-				<button className="operacion" onClick={calculate(props, this)} name="mult">x</button>
+				<button className="operation" onClick={calculate(props, this)} name="x">x</button>
 			</div>
 			<div>
 				<button onClick={isNumber()} name="4">4</button>
 				<button onClick={isNumber()} name="5">5</button>
 				<button onClick={isNumber()} name="6">6</button>
-				<button className="operacion" onClick={calculate(props, this)} name="resta">-</button>
+				<button className="operation" onClick={calculate(props, this)} name="-">-</button>
 			</div>
 			<div>
 				<button onClick={isNumber()} name="1">1</button>
 				<button onClick={isNumber()} name="2">2</button>
 				<button onClick={isNumber()} name="3">3</button>
-				<button className="operacion" onClick={calculate(props, this)} name="suma">+</button>
+				<button className="operation" onClick={calculate(props, this)} name="+">+</button>
 			</div>
 			<div>
 				<button className="dobleAncho" onClick={isNumber()} name="0">0</button>
 				<button onClick={calculate(props, this)} name="punto">.</button>
-				<button className="operacion" onClick={calculate(props, this)} name="igual">=</button>
+				<button className="operation" onClick={calculate(props, this)} name="=">=</button>
 			</div>
 		</div>
 
