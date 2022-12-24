bf2a94632168dbe493cefca2b2e4cd59315495bf
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2022-12-24 12:20:06 +0100

    CALCULADORA: Reconstrucción

--- calculator_isagonzalez/src/App.js
+++ calculator_isagonzalez/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './css/App.css';
+import calculate from "./logic/calculate";
+import CalculatorDisplay from "./components/CalculatorDisplay";
+import CalculatorButtons from "./components/CalculatorButtons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App {
+    state = {
+        total: null,
+        next: null,
+        operation: null
+    };
+
+    handleClick = buttonName => {
+        this.setState(calculate(this.state, buttonName));
+    }
+
+    render() {
+        return (
+            <div className="component-app">
+                <CalculatorDisplay value={this.state.next || this.state.total || "0"} />
+                <CalculatorButtons clickHandler={this.handleClick} />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ calculator_isagonzalez/src/components/CalculatorDisplay.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "../css/CalculatorDisplay.css";
+
+export default class CalculatorDisplay extends React.Component {
+    static propTypes = {
+        value: PropTypes.string,
+    };
+
+    render() {
+        return (
+            <div className="component-display">
+                <div>{this.props.value}</div>
+            </div>
+        );
+    }
+}
