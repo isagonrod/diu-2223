24441e05220092b7931a1af1d2808da494590c6e
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-01-17 19:14:18 +0100

    TUTORIALS: Add more files

---
+++ tutorials_isagonzalez/src/test/java/com/example/tutorials_isagonzalez/TutorialsIsagonzalezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorials_isagonzalez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsIsagonzalezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
