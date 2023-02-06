e9a81017ddcad3593a5c851c35251589e364eaac
isagonrod <isabelmariagonzalezrodriguez@gmail.com>
2023-02-06 14:26:41 +0100

    remove tutorials project

--- Front tutoriales react  V1/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "http://tut-igr.us-east-1.elasticbeanstalk.com",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
